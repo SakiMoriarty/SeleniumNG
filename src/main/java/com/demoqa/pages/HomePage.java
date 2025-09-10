a0ec5ff3c1f4c205748501565fd94c59c9a1c10d
Vitali.Pazniakou <jamesmoriarty8@yahoo.com>
2025-09-10 15:40:04 +0300

    Created tests for "demoqa": work with Tables

--- src/main/java/com/demoqa/pages/HomePage.java
+++ src/main/java/com/demoqa/pages/HomePage.java
@@ -2,6 +2,7 @@ package com.demoqa.pages;
 
 import com.demoqa.pages.Forms.FormsPage;
 import com.base.BasePage;
+import com.demoqa.pages.elements.ElementsPage;
 import org.openqa.selenium.By;
 
 import static utilities.JavaScriptUtility.scrollToElementJS;
@@ -9,10 +10,17 @@ import static utilities.JavaScriptUtility.scrollToElementJS;
 public class HomePage extends BasePage {
 
     private By formsCard = By.xpath("//div[@id='app']//h5[text()='Forms']");
+    private By elementsCard = By.xpath("//div[@id='app']//h5[text()='Elements']");
 
     public FormsPage goToForms(){
         scrollToElementJS(formsCard);
         click(formsCard);
         return new FormsPage();
+    }
+
+    public ElementsPage goToElements(){
+        scrollToElementJS(elementsCard);
+        click(elementsCard);
+        return new ElementsPage();
     }
 }
---
+++ src/test/java/com/demoqa/tests/part3/elements/WebTableTest.java
@@ -0,0 +1,21 @@
+package com.demoqa.tests.part3.elements;
+
+import com.demoqa.base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class WebTableTest extends BaseTest {
+
+    @Test
+    public void testWebTable(){
+        String email = "kierra@example.com";
+        String expectedAge = "34";
+
+        var webTablePage = homePage.goToElements().clickWebTables();
+        webTablePage.clickEdit(email);
+        webTablePage.setAge("34");
+        webTablePage.clickSubmitButton();
+        String actualAge = webTablePage.getTableAge(email);
+        Assert.assertEquals(actualAge, expectedAge, "Actual and Expected ages Do Not Match");
+    }
+}
