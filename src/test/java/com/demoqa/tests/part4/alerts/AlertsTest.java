51c3472c5244e4b96d264ed2a2db7c39a26be967
Vitali.Pazniakou <jamesmoriarty8@yahoo.com>
2025-09-11 12:48:42 +0300

    Created tests for "demoqa": work with Confirmation Alert

--- src/test/java/com/demoqa/tests/part4/alerts/AlertsTest.java
+++ src/test/java/com/demoqa/tests/part4/alerts/AlertsTest.java
@@ -4,12 +4,11 @@ import com.demoqa.base.BaseTest;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
-import static utilities.SwitchToUtility.acceptAlert;
-import static utilities.SwitchToUtility.getAlertText;
+import static utilities.SwitchToUtility.*;
 
+@Test
 public class AlertsTest extends BaseTest {
 
-    @Test
     public void testInformationAlert(){
         String expectedAlertText = "You clicked a button";
         var alertsPage = homePage.goToAlertsFramesWindowsCard().clickAlerts();
@@ -18,5 +17,15 @@ public class AlertsTest extends BaseTest {
         assertEquals(getAlertText(), expectedAlertText,
                 "\n Actual and Expected Message Do Not match");
         acceptAlert();
+    }
+
+    public void testConfirmationAlert(){
+        var alertsPage = homePage.goToAlertsFramesWindowsCard().clickAlerts();
+        alertsPage.clickConfirmationAlertButton();
+        dismissAlert();
+        String expectedConfirmationResult = "You selected Cancel";
+        String actualConfirmationResult = alertsPage.getConfirmationResult();
+        assertEquals(actualConfirmationResult, expectedConfirmationResult,
+                "\n Did not selected cancel");
     }
 }
